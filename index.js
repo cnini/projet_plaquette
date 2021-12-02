86bbf5db83545f2990ba6dcf38f3c6cca4ec59ac
dassimanuel000 <dassimanuel000@gmail.com>
2021-12-03 00:52:14 +0100

    Version semifinal  all & Dashboard

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 var url = "mongodb://localhost:27017/";
 
 var tasks = [];
 var completed = [];
 var status = "";
-var users = [];
 
+var users = [];
+var auth = [];
+
+var certificates = [];
+var partners = [];
+var partners_campus = [];
 // # Initialise users and usertypes
 
 MongoClient.connect(url, function(err, db) {
-	if (err) throw err;
-	var dbo = db.db("groupe6");
-	dbo.collection("users").find({}).toArray(function(err, result) {
-		if (err) throw err;
-		users = result;
-		db.close();
-		});
-	}); 
+    if (err) throw err;
+    var dbo = db.db("groupe6");
+    dbo.collection("users").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        users = result;
+        db.close();
+    });
+});
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("groupe6");
+    dbo.collection("certificate").find({}).toArray(function(err, results) {
+        if (err) throw err;
+        certificates = results;
+        db.close();
+    });
+});
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("groupe6");
+    var entreprise = { 'type': "entreprise" };
+
+    dbo.collection("partner").find(entreprise).toArray(function(err, result) {
+        if (err) throw err;
+        partners = result;
+        db.close();
+    });
+});
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("groupe6");
+    var campus = { 'type': "campus" };
+
+    dbo.collection("partner").find(campus).toArray(function(err, result) {
+        if (err) throw err;
+        partners_campus = result;
+        db.close();
+    });
+});
 
 //calling express
 var app = express();
@@ -28,75 +68,207 @@ app.set('view engine', 'ejs');
 //static data
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-	res.render('index', {status: status});
+app.get("/", function(req, res) {
+    res.render('index', { status: status });
 });
 
-app.get("/crud", function(req, res){
-	res.render('crud', {tasks: tasks , completed: completed});
+app.get("/crud", function(req, res) {
+    res.render('crud', { tasks: tasks, completed: completed });
 });
 
-app.get("/login", function(req, res){
-	res.render('login');
-});
-
-
-app.get("/dashboard", function(req, res){
-	res.render('dashboard/home', {users: users, });
+app.get("/login", function(req, res) {
+    res.render('login');
 });
 
 
-app.get("/certificate/edit", function(req, res){
-	res.render('certificate/edit');
+
+app.post("/login", function(req, res) {
+    if (auth.length == 1) {
+
+        res.redirect('/login?status=done');
+
+    } else {
+        if (req.body) {
+            MongoClient.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("groupe6");
+                var auths = { 'mail': req.body.mail, 'pass': req.body.pass };
+
+                dbo.collection("users").find(auths).toArray(function(err, result) {
+                    if (err) throw err;
+                    console.log(result);
+                    auth = result;
+                    db.close();
+                });
+                if (auth.length == 1) {
+                    res.redirect('/dashboard');
+                } else {
+                    res.redirect('/login?status=false');
+                }
+            });
+        }
+    }
 });
 
-app.post("/certificate/add", function(req, res){
-	if(req.body){
-		var certificate = [
-			{
-				"title":req.body.title,
-				"description":req.body.description,
-				"provider":req.body.provider,
-				"number": req.body.number,
-				"conditions": ""
-			}
-		]
-		MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("groupe6");
-		dbo.collection("users").insertMany(certificate, function(err, res) {
-			if (err) throw err;
-			console.log(res);
-			db.close();
-		  });
-		res.redirect('/dashboard');
-		});
-	}
+app.get("/dashboard", function(req, res) {
+    if (auth.length == 1) {
+        res.render('dashboard/home', {
+            auth: auth,
+            users: users,
+            certificates: certificates,
+            partners: partners,
+            partners_campus: partners_campus
+        });
+    } else {
+        res.redirect('/login?status=need')
+    }
+
 });
 
 
-app.post('/addtask', function(req, res){
-	if(req.body.newtask){
-		tasks.push(req.body.newtask);
-		res.redirect('/');
-	}
+app.post("/certificate/add", function(req, res) {
+    if (req.body) {
+        var certificate = [{
+            "title": req.body.title,
+            "description": req.body.description,
+            "provider": req.body.provider,
+            "duree": req.body.duree,
+            "conditions": ""
+        }];
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groupe6");
+            dbo.collection("certificate").insertMany(certificate, function(err, res) {
+                if (err) throw err;
+                console.log(res);
+                db.close();
+            });
+            res.redirect('/dashboard');
+        });
+    }
 });
 
-app.post('/addtask', function(req, res){
-	if(req.body.newtask){
-		tasks.push(req.body.newtask);
-		res.redirect('/');
-	}
+app.post("/certificate/edit/:id", function(req, res) {
+    if (req.body) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groupe6");
+            dbo.collection("certificate").updateOne({ _id: new ObjectID(req.params.id) }, { $set: { 'title': req.body.title, 'description': req.body.description, 'provider': req.body.provider, 'duree': req.body.duree } }, { upsert: true }, function(err, res) {
+                if (err) throw err;
+                console.log("1 document update");
+                console.log(res);
+                db.close();
+            });
+            res.redirect('/dashboard?status=done&m=Certificate update');
+        });
+    }
 });
 
-app.post('/removetask', function(req, res){
-	completed.push(req.body.check);
-	tasks.pop(req.body.check);
-	res.redirect('/');
+
+app.post("/certificate/delete/:id", function(req, res) {
+    if (req.body) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groupe6");
+
+            var myquery = { _id: req.params.id };
+            dbo.collection("certificate").deleteOne(myquery, function(err, obj) {
+                if (err) throw err;
+                console.log("1 document deleted", obj);
+                db.close();
+            });
+
+            console.log(myquery);
+
+            res.redirect('/dashboard?status=done&m=Certificate Delete');
+        });
+    }
 });
 
-app.listen(4455, function(){
-	console.log("Listening on port 4455");
+
+
+app.post("/partners/add", function(req, res) {
+    if (req.body) {
+        var partners = [{
+            "title": req.body.title,
+            "description": req.body.description,
+            "provider": req.body.provider,
+            "link": req.body.link,
+            "type": req.body.type,
+            "duree": req.body.duree,
+            "conditions": ""
+        }];
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groupe6");
+            dbo.collection("partner").insertMany(partners, function(err, res) {
+                if (err) throw err;
+                console.log(res);
+                db.close();
+            });
+            res.redirect('/dashboard');
+        });
+    }
+});
+
+app.post("/partners/edit/:id", function(req, res) {
+    if (req.body) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groupe6");
+            dbo.collection("partner").updateOne({ _id: new ObjectID(req.params.id) }, { $set: { 'title': req.body.title, 'description': req.body.description, 'provider': req.body.provider, 'link': req.body.link, 'type': req.body.type, 'duree': req.body.duree } }, { upsert: true }, function(err, res) {
+                if (err) throw err;
+                console.log("1 document update");
+                console.log(res);
+                db.close();
+            });
+            res.redirect('/dashboard?status=done&m=partners update');
+        });
+    }
+});
+
+
+app.post("/partners/delete/:id", function(req, res) {
+    if (req.body) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("groupe6");
+
+            var myquery = { _id: req.params.id };
+            dbo.collection("partner").deleteOne(myquery, function(err, obj) {
+                if (err) throw err;
+                console.log("1 document deleted", obj);
+                db.close();
+            });
+
+            console.log(myquery);
+
+            res.redirect('/dashboard?status=done&m=partners Delete');
+        });
+    }
+});
+
+app.post('/addtask', function(req, res) {
+    if (req.body.newtask) {
+        tasks.push(req.body.newtask);
+        res.redirect('/');
+    }
+});
+
+app.get('/adduser', function(req, res) {
+    var tools = require('./connectDB.js');
+    tools.init_admin();
+    res.redirect('/dashboard');
+});
+
+app.post('/removetask', function(req, res) {
+    completed.push(req.body.check);
+    tasks.pop(req.body.check);
+    res.redirect('/');
+});
+
+app.listen(4455, function() {
+    console.log("Listening on port 4455");
 });
