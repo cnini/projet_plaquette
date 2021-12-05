f258732410c8352b5cc4f8ad42e055362ed5481a
SadioToure <41106617+SadioToure@users.noreply.github.com>
2021-12-05 18:14:04 +0100

    Create userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,134 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { roles } = require('../roles')
+ 
+async function hashPassword(password) {
+ return await bcrypt.hash(password, 10);
+}
+ 
+async function validatePassword(plainPassword, hashedPassword) {
+ return await bcrypt.compare(plainPassword, hashedPassword);
+}
+ 
+exports.signup = async (req, res, next) => {
+    try {
+        const { email, password, role } = req.body
+        const hashedPassword = await hashPassword(password);
+        const newUser = new User({ email, password: hashedPassword, role: role || "basic" });
+        const accessToken = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d"
+        });
+        newUser.accessToken = accessToken;
+        await newUser.save();
+        res.json({
+        data: newUser,
+        accessToken
+        })
+    }   catch (error) {
+        next(error)
+        }
+}
+
+exports.login = async (req, res, next) => {
+    try {
+     const { email, password } = req.body;
+     const user = await User.findOne({ email });
+     if (!user) return next(new Error("Cet Email n'existe pas"));
+     const validPassword = await validatePassword(password, user.password);
+     if (!validPassword) return next(new Error('Mot de passe incorrect'))
+     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d"
+     });
+     await User.findByIdAndUpdate(user._id, { accessToken })
+     res.status(200).json({
+      data: { email: user.email, role: user.role },
+      accessToken
+     })
+    } catch (error) {
+     next(error);
+    }
+}
+
+
+exports.getUsers = async (req, res, next) => {
+    const users = await User.find({});
+    res.status(200).json({
+     data: users
+    });
+}
+    
+exports.getUser = async (req, res, next) => {
+    try {
+     const userId = req.params.userId;
+     const user = await User.findById(userId);
+     if (!user) return next(new Error('Cet utilisateur n\'existe pas'));
+      res.status(200).json({
+      data: user
+     });
+    } catch (error) {
+     next(error)
+    }
+}
+    
+exports.updateUser = async (req, res, next) => {
+    try {
+        const update = req.body
+        const userId = req.params.userId;
+        await User.findByIdAndUpdate(userId, update);
+        const user = await User.findById(userId)
+        res.status(200).json({
+        data: user,
+        message: 'L\'utilisateur a été mis à jour'
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+    
+exports.deleteUser = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        await User.findByIdAndDelete(userId);
+        res.status(200).json({
+        data: null,
+        message: 'L\'utilisateur a été supprimé'
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+exports.grantAccess = function(action, resource) {
+    return async (req, res, next) => {
+        try {
+            const permission = roles.can(req.user.role)[action](resource);
+            if (!permission.granted) {
+            return res.status(401).json({
+                error: "Vous ne disposez pas des autorisations suffisantes pour effectuer cette action"
+            });
+            }
+            next()
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+    
+exports.allowIfLoggedin = async (req, res, next) => {
+    try {
+        const user = res.locals.loggedInUser;
+        if (!user)
+        return res.status(401).json({
+        error: "Vous devez être connecté pour accéder à cette route"
+        });
+        req.user = user;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+
